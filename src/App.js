2ee0524f8c16624349e352752c1490fbbfef773d
Joey <jspahn83@gmail.com>
2020-06-11 21:47:03 -0600

    create folder structure and install react router, set up routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import NewPlace from './places/pages/new-place';
+import Users from './user/pages/users';
+
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' >
+          <Users />
+        </Route>
+        <Route exact path='/places/new'>
+          <NewPlace />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    </Router>
   );
 }
 
